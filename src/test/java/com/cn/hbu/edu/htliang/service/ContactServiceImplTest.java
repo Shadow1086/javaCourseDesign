e565f96d24e986554665357853c77c80ad102cb0
Liang haotian <l15304704058@126.com>
2025-12-14 19:13:19 +0800

    ✨ feat: 新增联系人服务实现类的单元测试

---
+++ src/test/java/com/cn/hbu/edu/htliang/service/ContactServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.cn.hbu.edu.htliang.service;
+
+import org.junit.Test;
+
+/**
+ * 类名: ContactServiceImplTest
+ * 创建时间: 2025/12/14 18:31
+ * 项目描述:
+ *
+ * @author htLiang
+ */
+public class ContactServiceImplTest {
+
+    @Test
+    public void addContact() {
+    }
+
+    @Test
+    public void findId() {
+    }
+
+    @Test
+    public void findName() {
+    }
+
+    @Test
+    public void deleteId() {
+    }
+
+    @Test
+    public void updateContactInfo() {
+    }
+}
