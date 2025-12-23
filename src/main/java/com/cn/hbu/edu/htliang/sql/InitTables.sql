d1614f6bd0eacb57c387f748a9b3f2b703b0c76c
Liang haotian <l15304704058@126.com>
2025-12-23 20:37:18 +0800

    feat(sql): 优化数据库表结构及索引，增强数据完整性和查询性能
    - 增加多张表的删除语句，支持重新初始化数据库
    - 手动开启SQLite外键约束，确保数据关联完整性
    - 为contacts表新增created_at和updated_at时间戳字段
    - 添加触发器实现updated_at字段的自动更新时间
    - 创建多张索引提升基于姓名、电话、组名和标签的查询效率
    - 组织表结构及注释，提升脚本的可读性和维护性

--- src/main/java/com/cn/hbu/edu/htliang/sql/InitTables.sql
+++ src/main/java/com/cn/hbu/edu/htliang/sql/InitTables.sql
@@ -1,4 +1,15 @@
-drop table if exists Contacts;
+-- 删除所有表结构，慎重！！！！！！
+drop table if exists  contacts_group;
+drop table if exists  tag_contacts;
+drop table if exists  tags;
+drop table if exists  groups;
+drop table if exists contacts;
+
+
+-- SQLite 默认关闭外键约束，需手动开启
+pragma foreign_keys = on;
+pragma foreign_keys; -- 返回 1 表示已启用
+
 -- 初始化多对多表之间的关联关系
 create table contacts
 (
@@ -8,8 +19,20 @@ create table contacts
     tele2 varchar(20),
     home  varchar(100),
     email varchar(50),
-    notes varchar(200)
+    notes varchar(200),
+    created_at datetime default current_timestamp,
+    updated_at datetime default current_timestamp
 );
+-- 添加触发器
+create trigger update_contacts_timestamp
+    after update on contacts
+begin
+    update contacts set updated_at = current_timestamp
+        where id = new.id;
+end;
+
+create index idx_contacts_name on contacts(name);
+create index idx_contacts_tele2 on contacts(tele2);
 
 create table groups
 (
@@ -24,6 +47,10 @@ create table contacts_group
     contacts_id integer not null references contacts (id) on update cascade on delete cascade,
     unique (group_id, contacts_id)
 );
+
+-- 创建索引为了加快按照组名查询联系人
+create index idx_groups_group_name on groups(group_name);
+
 create table tags
 (
     id        integer primary key autoincrement,
@@ -38,10 +65,9 @@ create table tag_contacts
     tag_id      integer not null references tags (id) on update cascade on delete cascade,
     unique (contacts_id, tag_id)
 );
--- 查看外键信息
-pragma foreign_key_list(contacts_group);
-pragma foreign_key_list(tag_contacts);
--- SQLite 默认关闭外键约束，需手动开启
-pragma foreign_keys = on;
-pragma foreign_keys; -- 返回 1 表示已启用
+
+-- 创建索引，加快按照标签查询联系人
+create index idx_tags_tag_name on tags(tag_name);
+create index idx_tags_contacts_tag_id on tag_contacts(tag_id);
+
 
